0a070bd52d649c3669cbf17b74c90611bbe33b60
Sergio Piedrahita <sergiopiedrahitam@gmail.com>
2025-02-02 09:05:50 -0500

    MAnejo estados y condiciones ternarias

--- Components/Player/Player.jsx
+++ Components/Player/Player.jsx
@@ -1,14 +1,31 @@
+import { use } from "react";
 import "./Player.css"
+import { useState } from "react";
 
 export default function Player({playerName, playerSimbol, children}){
+    const [isEditing,setIsEditing]= useState(false)
+
+    function handleChangeClick(){
+        setIsEditing(!isEditing);
+        // asi se cambia el valor del booleano cada vez que hagas click, dependiento del valor que tenga
+      }
+    
+    const playerNameField = isEditing
+    ? (<input type="text" name="" id="" required/>)
+    : (<span className='player-name'>{playerName}</span>)
+
+    const buttonChangeValue = isEditing
+    ? "Guardar"
+    : "Cambiar"
+
     return (
         <>
             <li>
                 <span className='player'>
-                    <span className='player-name'>{playerName}</span>
+                    {playerNameField}
                     <span className='player-simbol'>{playerSimbol}</span>
                 </span>
-                <button>Cambiar</button>
+                <button onClick={()=>handleChangeClick()}>{buttonChangeValue}</button>
             </li>
         </>
     );
